884879d7aa6e6e6227ef24c277a9325b64fd5d98
xmbsm <106425564+xmbsm@users.noreply.github.com>
2025-07-19 22:41:14 +0800

    Update raindrop.tsx

--- lib/raindrop.tsx
+++ lib/raindrop.tsx
@@ -1,1 +1,22 @@
+import "server-only";
+import { cache } from "react";
 
+const options = {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_RAINDROP_ACCESS_TOKEN}`,
+  },
+};
+
+const RAINDROP_API_URL = "https://api.raindrop.io/rest/v1";
+export const getCollections = cache(async () => {
+  try {
+    const response = await fetch(`${RAINDROP_API_URL}/collections`, options);
+    const collections = await response.json();
+    return collections;
+  } catch (error) {
+    console.info(error);
+    return null;
+  }
+});
